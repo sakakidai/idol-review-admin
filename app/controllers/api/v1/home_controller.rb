d930f38773750045bd6b7957314461fc9dd47a32
sakakidai <sd.lybd1016@gmail.com>
2021-10-05 09:28:39 +0900

    add home controller

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,32 +1,24 @@
 module Api
   module V1
-    class BlogsController < ApiController
-      before_action :set_blog, only: %i[ show ]
-
+    class HomeController < ApiController
       def index
-        blogs = params[:tag].blank? ? Blog.all : Blog.tagged_with(params[:tag])
-        render json: blogs, each_serializer: Api::V1::BlogSerializer
-      end
-
-      def show
+        blogs = Blog.all
+        idols = Idol.all
         genre_list = Blog.tags_on(:genres)
         distributor_list = Blog.tags_on(:distributors)
 
         # include: [:idol, :content_images] は省略
-        serialized_blog = ActiveModelSerializers::SerializableResource.new(@blog, serializer: Api::V1::BlogSerializer, template: 'show')
+        serialized_blogs = ActiveModelSerializers::SerializableResource.new(blogs, each_serializer: Api::V1::BlogSerializer, template: 'index')
+        serialized_idols = ActiveModelSerializers::SerializableResource.new(idols, each_serializer: Api::V1::IdolSerializer)
         serialized_genre_list = ActiveModelSerializers::SerializableResource.new(genre_list, each_serializer: Api::V1::TagSerializer)
         serialized_distributor_list = ActiveModelSerializers::SerializableResource.new(distributor_list, each_serializer: Api::V1::TagSerializer)
 
         render json: {
-          blog: serialized_blog,
+          blogs: serialized_blogs,
+          idols: serialized_idols,
           genreList: serialized_genre_list,
           distributorList: serialized_distributor_list,
         }
-      end
-
-      private
-      def set_blog
-        @blog = Blog.find(params[:id])
       end
     end
   end
