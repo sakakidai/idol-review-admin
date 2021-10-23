d09d7055c4d27e19dd0091409e04df5d26b82cae
sakakidai <sd.lybd1016@gmail.com>
2021-10-23 15:42:25 +0900

    fix nginx configure

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("IDOL_REVIEW_CUSTOMER") { 'localhost:3000' }
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: true
+    # origins ENV.fetch("IDOL_REVIEW_CUSTOMER") { 'localhost:3000' }
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: false
   end
 end
